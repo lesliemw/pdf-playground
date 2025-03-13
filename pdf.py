2934eed8801dfb54c03f1565df7c8b3c3c42fba0
lesliemw <l.marie1598@gmail.com>
2025-03-13 21:30:27 +0000

    update file

--- pdf.py
+++ pdf.py
@@ -1,17 +1,31 @@
 import PyPDF2
-import sys
+# import sys
 
-inputs = sys.argv[1:]
-
-
-def pdf_combiner(pdf_list):
-    merger = PyPDF2.PdfFileMerger()
-    for pdf in pdf_list:
-        merger.append(pdf)
-    merger.write("super.pdf")
+# inputs = sys.argv[1:]
 
 
-pdf_combiner(inputs)
+# def pdf_combiner(pdf_list):
+#     '''combines all the pdfs in the list into a single pdf'''
+#     merger = PyPDF2.PdfFileMerger()
+#     for pdf in pdf_list:
+#         merger.append(pdf)
+#     merger.write("super.pdf")
+
+
+# pdf_combiner(inputs)
+
+
+template = PyPDF2.PdfFileReader(open("super.pdf", "rb"))
+watermark = PyPDF2.PdfFileReader(open("wtr.pdf", "rb"))
+output = PyPDF2.PdfFileWriter()
+
+for i in range(template.getNumPages()):
+    page = template.getPage(i)
+    page.mergePage(watermark.getPage(0))
+    output.addPage(page)
+    with open("watermarked_output.pdf", "wb") as file:
+        output.write(file)
+
 
 # with open("dummy.pdf", "rb") as file:
 #     reader = PyPDF2.PdfFileReader(file)
