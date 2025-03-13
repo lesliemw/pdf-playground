2a9028d4bb5de40e1d3ae6cabce60dd628bdb857
lesliemw <l.marie1598@gmail.com>
2025-03-13 21:20:29 +0000

    update/add file

--- pdf.py
+++ pdf.py
@@ -1,10 +1,23 @@
 import PyPDF2
+import sys
 
-with open("dummy.pdf", "rb") as file:
-    reader = PyPDF2.PdfFileReader(file)
-    page = reader.getPage(0)
-    page.rotateCounterClockwise(90)
-    with open("tilt.pdf", "wb") as file:
-        writer = PyPDF2.PdfFileWriter()
-        writer.addPage(page)
-        writer.write(file)
+inputs = sys.argv[1:]
+
+
+def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfFileMerger()
+    for pdf in pdf_list:
+        merger.append(pdf)
+    merger.write("super.pdf")
+
+
+pdf_combiner(inputs)
+
+# with open("dummy.pdf", "rb") as file:
+#     reader = PyPDF2.PdfFileReader(file)
+#     page = reader.getPage(0)
+#     page.rotateCounterClockwise(90)
+#     with open("tilt.pdf", "wb") as file:
+#         writer = PyPDF2.PdfFileWriter()
+#         writer.addPage(page)
+#         writer.write(file)
